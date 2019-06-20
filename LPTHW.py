4eeeb8e1dc92a49d9238bdca8c79c16715255ff0
pushpinder <pushpinders555@gmail.com>
2019-06-20 14:22:09 +0530

    Make desisions game

--- LPTHW.py
+++ LPTHW.py
@@ -1,7 +1,7 @@
 
 '''print "Hello World!"
 print "Hello Again"
-print "I like ytping this."
+print "I like typing this."
 print "This is fun."
 print "Yay! Printing."
 print "i'd much rather you not"
@@ -468,3 +468,36 @@ print 'I "said" do not touch this.'
 
 # else:
 #     print("Fine, let's stay home then")
+
+#-----MAKING DECISIONS ------
+
+print("You enter a dark room with two doors. Do you go through door no-1 or door no-2?")
+
+door = input('> ')
+
+if door == "1":
+    print("There's a giant vear here eating a cheese cake. What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your leg off. Good job!")
+    else:
+        print("Well, doing %s is probably better. Bear runs away." % bear)
+
+elif door == "2":
+    print("You stare into the endless abyss at Cthulu's retina.")
+    print("1. Blueberries.")
+    print("2. Yellow jacket clothespines.")
+    print("3. Understanding revolvers yelling melodies.")
+
+    insanity = input('> ')
+
+    if insanity == "1" or insanity == "2":
+        print ("Your body survives powered by a mind of jello. Good job!")
+    else:    
+        print("You stumble around and fall on a knife and die. Good job!")

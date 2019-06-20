7bacb1b4966e09b86623482079867966766ccca1
pushpinder <pushpinders555@gmail.com>
2019-06-20 14:27:04 +0530

    add space

--- LPTHW.py
+++ LPTHW.py
@@ -471,33 +471,33 @@ print 'I "said" do not touch this.'
 
 #-----MAKING DECISIONS ------
 
-print("You enter a dark room with two doors. Do you go through door no-1 or door no-2?")
+# print("You enter a dark room with two doors. Do you go through door no-1 or door no-2?")
 
-door = input('> ')
+# door = input('> ')
 
-if door == "1":
-    print("There's a giant vear here eating a cheese cake. What do you do?")
-    print("1. Take the cake.")
-    print("2. Scream at the bear.")
+# if door == "1":
+#     print("There's a giant vear here eating a cheese cake. What do you do?")
+#     print("1. Take the cake.")
+#     print("2. Scream at the bear.")
 
-    bear = input("> ")
+#     bear = input("> ")
 
-    if bear == "1":
-        print("The bear eats your face off. Good job!")
-    elif bear == "2":
-        print("The bear eats your leg off. Good job!")
-    else:
-        print("Well, doing %s is probably better. Bear runs away." % bear)
+#     if bear == "1":
+#         print("The bear eats your face off. Good job!")
+#     elif bear == "2":
+#         print("The bear eats your leg off. Good job!")
+#     else:
+#         print("Well, doing %s is probably better. Bear runs away." % bear)
 
-elif door == "2":
-    print("You stare into the endless abyss at Cthulu's retina.")
-    print("1. Blueberries.")
-    print("2. Yellow jacket clothespines.")
-    print("3. Understanding revolvers yelling melodies.")
+# elif door == "2":
+#     print("You stare into the endless abyss at Cthulu's retina.")
+#     print("1. Blueberries.")
+#     print("2. Yellow jacket clothespines.")
+#     print("3. Understanding revolvers yelling melodies.")
 
-    insanity = input('> ')
+#     insanity = input('> ')
 
-    if insanity == "1" or insanity == "2":
-        print ("Your body survives powered by a mind of jello. Good job!")
-    else:    
-        print("You stumble around and fall on a knife and die. Good job!")
+#     if insanity == "1" or insanity == "2":
+#         print ("Your body survives powered by a mind of jello. Good job!")
+#     else:    
+#         print("You stumble around and fall on a knife and die. Good job!")

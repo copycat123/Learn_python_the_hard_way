f9aa9037196015b3a186b1943ea22d3ee98474ee
pushpinder <pushpinders555@gmail.com>
2019-06-20 14:53:55 +0530

    list with loops

--- LPTHW.py
+++ LPTHW.py
@@ -501,3 +501,27 @@ print 'I "said" do not touch this.'
 #         print ("Your body survives powered by a mind of jello. Good job!")
 #     else:    
 #         print("You stumble around and fall on a knife and die. Good job!")
+
+#---------LIST WITH LOOPS------------
+
+# The_count = [1, 2, 3, 4, 5]
+# fruits = ['apples', 'oranges', 'pears', 'apricots']
+# change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# for number in The_count:
+#     print("This is count %d" % number)
+
+# for fruit in fruits:
+#     print("A fruit of type: %s" % fruit)
+
+# for i in change:
+#     print("I got %r" % i)
+
+# elements = []
+
+# for i in range(0, 6):
+#     print("Adding %d to the list." % i)
+#     elements.append(i)
+
+# for i in elements:
+#     print("Element was: %d" % i)
